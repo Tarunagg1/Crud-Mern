c07aaa7b615db4841c4f628e27b46a1c71d9d5d0
Tarun Aggarwal <48708852+Tarunagg1@users.noreply.github.com>
2020-10-02 12:02:13 +0530

    Add files via upload

---
+++ react/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import reactDom from 'react-dom'
+import App from './App'
+import  './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import {BrowserRouter} from 'react-router-dom'
+
+
+
+reactDom.render(
+    <>
+    <BrowserRouter>
+      <App/> 
+      </BrowserRouter>
+    </>
+    , document.getElementById("root"))
+
+
